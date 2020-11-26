79b1b6e0d0c18d4ed801c0cba87ddae466f84115
김수영 <ymt45@estsecurity.com>
2020-11-26 10:59:51 +0900

    리액트 앱 parcel로 번들링

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import { Switch } from 'react-router-dom';
+
+import './style.css';
+
+const App = () => {
+  return <div>App with parcel</div>;
+};
+
+export default App;
